2c50c1a5016dd5f5cfca3dd80c479e9f4afec0a5
sharkfinn23 <thomashackett@hanovernorwichschools.org>
2016-09-27 14:23:54 -0400

    Updated from Brython Server: 9/27/2016 2:23:54 PM

--- distribution.py
+++ distribution.py
@@ -36,13 +36,17 @@ Notice about this example:
   in the text and they are listed in the output in alphabetical order.
 * Letters that do not occur in the text are not listed in the output at all.
 """
+import string
 string=input("Please enter a string of text (the bigger the better): ")
 print('The distribution of characters in "'+string+'" is: ')
 list=list(string)
 list1=[]
 list1.extend([list.count('a'), list.count('b'), list.count('c'), list.count('d'), list.count('e'), list.count('f'), list.count('g'), list.count('h'), list.count('i'), list.count('j'), list.count('k'), list.count('l'), list.count('m'), list.count('n'), list.count('o'), list.count('p'), list.count('q'), list.count('r'), list.count('s'), list.count('t'), list.count('u'), list.count('v'), list.count('w'), list.count('x'), list.count('y'), list.count('z'),])
-list2=list(range(0,26))
-list3=list2[::-1]
+list2 = [x for x in range(1,27)]
+list3 = list2[::-1]
+alphaList = list(string.ascii_lowercase)
 print(list2)
-print(list1)
+print(list3)
+print(alphaList)
+
     
