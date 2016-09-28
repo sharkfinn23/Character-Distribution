ccdb6806fc49c3ae400c0e664908835f0e7bf58b
sharkfinn23 <thomashackett@hanovernorwichschools.org>
2016-09-28 12:57:24 -0400

    Updated from Brython Server: 9/28/2016 12:57:24 PM

--- distribution.py
+++ distribution.py
@@ -36,7 +36,6 @@ Notice about this example:
   in the text and they are listed in the output in alphabetical order.
 * Letters that do not occur in the text are not listed in the output at all.
 """
-import string
 astring=(input("Please enter a string of text (the bigger the better): "))
 print('The distribution of characters in "'+astring+'" is: ')
 list=list(astring)
